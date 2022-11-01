897d80ce803e6c0d2d483c151dd6024c885aec34
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-01 18:27:15 +0530

    understanding request and response object

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const http=require('http');
 
 const server=http.createServer((request,response)=>{
-console.log(request.url)
-response.end("hello Nodejs")
+    if(request.url==='/about'){
+        return response.end("The about Page")
+    }else if(request.url==='/contact'){
+        return response.end("The contact Page")
+    }else if(request.url==='/'){
+        return response.end("The home Page")
+    }else{
+        response.writeHead(404)
+        response.end("The page was not found")
+    }
 
 })
 server.listen(3000)
