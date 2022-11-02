ec02937a743726ba0d7cb04bf70f935e59973b2c
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-02 10:36:04 +0530

    express-framework

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
-const http=require('http');
-const fs=require('fs');
+const express = require("express");
+const app = express();
 
-const aboutPage=fs.readFile('about.html',(file)=>{
-
-});
-
-// Non blocking
-const contactPage=fs.readFileSync('contact.html');
-const homePage=fs.readFileSync('index.html');
-
-const server=http.createServer((request,response)=>{
-    if(request.url==='/about'){
-        return response.end(aboutPage)
-    }else if(request.url==='/contact'){
-        return response.end(contactPage)
-    }else if(request.url==='/'){
-        return response.end(homePage)
-    }else{
-        response.writeHead(404)
-        response.end("The page was not found")
-    }
-
+app.get('/',(request,response)=>{
+    response.json({
+        name:"Princy"
+    })
 })
-server.listen(3000)
+
+
+app.get('/',(request,response)=>{
+    response.send({
+        name:"Princy"
+    })
+})
+
+app.listen(3000,()=>{
+    console.log("App is listing on port 3000")
+})
