af643fd01084683caadf816f5020e26476d2a858
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-04 11:15:02 +0530

    creating-the-home-page

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const path =require("path")
 const app = express();
 
+app.use(express.static('public'))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
 
 app.listen(3000,()=>{
     console.log("App is listing on port 3000")
