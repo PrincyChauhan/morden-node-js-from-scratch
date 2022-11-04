b969ade56dc3d9941a6fc212ae7f71d08cdb6e02
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-04 10:55:14 +0530

    installing project file

--- index.js
+++ index.js
@@ -1,18 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get('/',(request,response)=>{
-    response.json({
-        name:"Princy"
-    })
-})
-
-
-app.get('/',(request,response)=>{
-    response.send({
-        name:"Princy"
-    })
-})
 
 app.listen(3000,()=>{
     console.log("App is listing on port 3000")
