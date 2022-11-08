317ee562421e98283bd4d5f0f15dd6ab5c6047a5
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-08 15:48:00 +0530

    about-conatct-post-page

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ app.get('/',(req,res)=>{
     res.sendFile(path.resolve(__dirname,'pages/index.html'))
 })
 
+app.get('/about',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+})
+
+
+app.get('/post',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+})
+
+app.get('/contact',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+})
 app.listen(3000,()=>{
     console.log("App is listing on port 3000")
 })
