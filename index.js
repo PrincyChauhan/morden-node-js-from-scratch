c825bfe8919b0e17e3a62c861a31bd2a7c1cf990
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-08 16:46:40 +0530

    templateing

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const express = require("express");
-const path =require("path")
+const path = require("path");
+const { config, engine } = require('express-edge');
+ 
 const app = express();
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/index.html'))
-})
+// Automatically sets view engine and adds dot notation to app.render
+app.use(engine);
+app.set('views', `${__dirname}/views`);
 
-app.get('/about',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/about.html'))
-})
+app.get("/", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "pages/index.html"));
+  res.render("index");
+});
 
+app.get("/about", (req, res) => {
+    res.render("about");
+});
 
-app.get('/post',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/post.html'))
-})
+app.get("/post", (req, res) => {
+    res.render("post");
+});
 
-app.get('/contact',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
-})
-app.listen(3000,()=>{
-    console.log("App is listing on port 3000")
-})
+app.get("/contact", (req, res) => {
+    res.render("contact");
+});
+app.listen(3000, () => {
+  console.log("App is listing on port 3000");
+});
