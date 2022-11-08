1e19f52a79ad3a80d6364603a7eb759e517b2432
PrincyChauhan <chauhanprincee7@gmail.com>
2022-11-08 17:04:21 +0530

    mongodb connection

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const { config, engine } = require('express-edge');
+const mongoose=require("mongoose");
+
  
 const app = express();
 
@@ -9,6 +11,15 @@ app.use(express.static("public"));
 // Automatically sets view engine and adds dot notation to app.render
 app.use(engine);
 app.set('views', `${__dirname}/views`);
+
+mongoose
+  .connect('mongodb://localhost:27017/blogs')
+  .then(() => {
+    console.log("Successfully connected ");
+  })
+  .catch((error) => {
+    console.log(`can not connect to database, ${error}`);
+  });
 
 app.get("/", (req, res) => {
   //   res.sendFile(path.resolve(__dirname, "pages/index.html"));
